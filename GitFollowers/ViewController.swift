5dd203760d28d8a8feca8b51aca44ca34abd2e36
Adam Moskovich <adamam@gmail.com>
2020-04-26 19:39:44 -0600

    Initial Commit

---
+++ GitFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitFollowers
+//
+//  Created by Adam Moskovich on 4/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
