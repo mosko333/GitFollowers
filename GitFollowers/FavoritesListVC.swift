f7d77686fdd71042537b6109514ed569e90c88d4
Adam Moskovich <adamam@gmail.com>
2020-05-09 21:38:53 -0600

    Adds SearchVC and FavoritesListVC

---
+++ GitFollowers/FavoritesListVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesListVC.swift
+//  GitFollowers
+//
+//  Created by Adam Moskovich on 5/9/20.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ GitFollowers/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  GitFollowers
+//
+//  Created by Adam Moskovich on 5/9/20.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+}
