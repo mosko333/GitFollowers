c581a1f59f913b3baf365b20e99ff1a517678fc1
Adam Moskovich <adamam@gmail.com>
2020-05-14 21:51:05 -0600

    SearchVC: Setup

--- GitFollowers/SearchVC.swift
+++ GitFollowers/SearchVC.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class SearchVC: UIViewController {
 
+    let logImageView = UIImageView()
+    let userNameTextField = GFTextField()
+    let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemBackground
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
 }
