f666c33a9c6ceea0b20f1fec32e07418f051dfa9
Adam Moskovich <adamam@gmail.com>
2020-05-17 20:08:43 -0600

    FollowerListVC: Adds file

---
+++ GitFollowers/FollowerListVC.swift
@@ -0,0 +1,29 @@
+//
+//  FollowerListVC.swift
+//  GitFollowers
+//
+//  Created by Adam Moskovich on 5/17/20.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
