2341dee15339b59475591048763c8322bd8acd3a
Adam Moskovich <adamam@gmail.com>
2020-05-24 22:46:56 -0600

    GFTitleLabel: Adds File

---
+++ GitFollowers/GFTitleLabel.swift
@@ -0,0 +1,20 @@
+//
+//  GFTitleLabel.swift
+//  GitFollowers
+//
+//  Created by Adam Moskovich on 5/24/20.
+//
+
+import UIKit
+
+class GFTitleLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
