7f9578845626cb5becdbf4363d63924ea6f80f1d
Adam Moskovich <adamam@gmail.com>
2020-05-24 22:57:03 -0600

    GFBodyLabel: Adds file

---
+++ GitFollowers/GFBodyLabel.swift
@@ -0,0 +1,20 @@
+//
+//  GFBodyLabel.swift
+//  GitFollowers
+//
+//  Created by Adam Moskovich on 5/24/20.
+//
+
+import UIKit
+
+class GFBodyLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
