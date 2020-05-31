dc52f7b520ba7a8526f43f417a1942623d628038
Adam Moskovich <adamam@gmail.com>
2020-05-30 18:44:56 -0600

    UIViewController+Ext: Adds present alert method

--- GitFollowers/UIViewController+Ext.swift
+++ GitFollowers/UIViewController+Ext.swift
@@ -9,4 +9,12 @@ import UIKit
 
 extension UIViewController {
 
+    func pressentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
 }
