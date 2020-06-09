3c3061aaa14229b3af2bd9c6a3605fc8e818d34a
Adam Moskovich <adamam@gmail.com>
2020-06-08 22:23:13 -0600

    NetworkManager: Fix baseUrl

--- GitFollowers/Managers/NetworkManager.swift
+++ GitFollowers/Managers/NetworkManager.swift
@@ -14,7 +14,7 @@ class NetworkManager {
     private init() {}
 
     func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, String?) -> Void) {
-        let endpoint = baseURL + "/users/\(username)/followers?per_page=100&page=\(page)"
+        let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
 
         guard let url = URL(string: endpoint) else {
             completed(nil, "This username created an invalid request. Please try again.")
@@ -23,10 +23,10 @@ class NetworkManager {
 
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
 
-            if let _ = error {
+            if error != nil {
                 completed(nil, "Unable to complete your request. Please check your internet connection")
             }
-            
+
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
                 completed(nil, "Invalid response from the server. Please try again.")
                 return
