4d929c8620a69f482d6ac1df2345148c4c0cadd9
Adam Moskovich <adamam@gmail.com>
2020-06-08 18:50:39 -0600

    Follower: Adds file and model

---
+++ GitFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitFollowers
+//
+//  Created by Adam Moskovich on 6/8/20.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
