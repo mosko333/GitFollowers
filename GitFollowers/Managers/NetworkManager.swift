a46748787d1a20c43f27378c60d7672a52e45eec
Adam Moskovich <adamam@gmail.com>
2020-06-09 21:24:36 -0600

    NetworkManager: return localized description

--- GitFollowers/Managers/NetworkManager.swift
+++ GitFollowers/Managers/NetworkManager.swift
@@ -43,7 +43,7 @@ class NetworkManager {
                 let followers = try decoder.decode([Follower].self, from: data)
                 completed(followers, nil)
             } catch {
-                completed(nil, "The data received from the server was invalid. Please try again.")
+                completed(nil, error.localizedDescription)
             }
         }
 
