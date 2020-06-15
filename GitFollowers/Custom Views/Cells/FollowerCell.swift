46db39d5e7697dfcb35f11c9273d284eb232167b
Adam Moskovich <adamam@gmail.com>
2020-06-14 20:44:11 -0600

    FollowerCell: Adds File

---
+++ GitFollowers/Custom Views/Cells/FollowerCell.swift
@@ -0,0 +1,15 @@
+//
+//  FollowerCell.swift
+//  GitFollowers
+//
+//  Created by Adam Moskovich on 6/14/20.
+//
+
+import UIKit
+
+class FollowerCell: UITableViewCell {
+    static let reuseID = "FollowerCell"
+
+    
+    
+}
