a50511cca22f953d731bc3f91840f27a56856e11
Adam Moskovich <adamam@gmail.com>
2020-06-14 21:07:30 -0600

    FollowerCell: adds Set method

--- GitFollowers/Custom Views/Cells/FollowerCell.swift
+++ GitFollowers/Custom Views/Cells/FollowerCell.swift
@@ -22,6 +22,10 @@ class FollowerCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
 
+    func set(follower: Follower) {
+        usernameLabel.text = follower.login
+    }
+
     private func configure() {
         addSubview(avatarImageView)
         addSubview(usernameLabel)
