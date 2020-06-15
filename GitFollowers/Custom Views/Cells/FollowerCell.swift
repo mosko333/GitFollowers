bc583f13b8d37759c2e9d6b45850d36552cb14e4
Adam Moskovich <adamam@gmail.com>
2020-06-14 20:56:09 -0600

    FollowersCell: Changes Cell to CollectionViewCell and adds init and GFAvatarImageView

--- GitFollowers/Custom Views/Cells/FollowerCell.swift
+++ GitFollowers/Custom Views/Cells/FollowerCell.swift
@@ -7,10 +7,22 @@
 
 import UIKit
 
-class FollowerCell: UITableViewCell {
+class FollowerCell: UICollectionViewCell {
     static let reuseID = "FollowerCell"
 
-    let avatarImageView = UIImageView()
+    let avatarImageView = GFAvatarImageView(frame: .zero)
     let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
-    
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configure() {
+
+    }
 }
