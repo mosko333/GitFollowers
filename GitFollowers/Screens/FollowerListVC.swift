7a5a90f992f0ae1411e3de8392fec7c9a2e1f7b3
Adam Moskovich <adamam@gmail.com>
2020-06-14 22:22:53 -0600

    FollowerListVC: Adds Collection view and config methods

--- GitFollowers/Screens/FollowerListVC.swift
+++ GitFollowers/Screens/FollowerListVC.swift
@@ -10,13 +10,35 @@ import UIKit
 class FollowerListVC: UIViewController {
 
     var userName: String!
+    var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCollectionView()
+        configureViewController()
+        getFollowers()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+
+    private func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
 
+    private func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+
+    private func getFollowers() {
         NetworkManager.shared.getFollowers(for: userName, page: 1) { [weak self] result in
+
             switch result {
             case .success(let followers):
                 print(followers)
@@ -26,10 +48,5 @@ class FollowerListVC: UIViewController {
                                                  buttonTitle: "OK")
             }
         }
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: true)
     }
 }
