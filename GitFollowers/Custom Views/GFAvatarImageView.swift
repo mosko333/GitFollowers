bfd543043b73f19f887835dfe089dd83129b60ba
Adam Moskovich <adamam@gmail.com>
2020-06-14 20:49:56 -0600

    GFAvatarImageView: Adds file

---
+++ GitFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,24 @@
+//
+//  GFAvatarImageView.swift
+//  GitFollowers
+//
+//  Created by Adam Moskovich on 6/14/20.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configure() {
+        
+    }
+}
