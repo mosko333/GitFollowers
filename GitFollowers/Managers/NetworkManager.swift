6390925944ca44a8f48a94bc3d01ed163f66ede9
Adam Moskovich <adamam@gmail.com>
2020-07-04 23:37:24 -0600

    NetworkManager: Adds Cache

--- GitFollowers/Managers/NetworkManager.swift
+++ GitFollowers/Managers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by Adam Moskovich on 6/8/20.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com/users/"
+    private let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
 
     private init() {}
 
